ab6c16edb44da0790aca081d863fc6276f46e278
dwlam8790 <49571438+dwlam8790@users.noreply.github.com>
2019-10-28 22:19:10 -0400

    added all folders and js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
